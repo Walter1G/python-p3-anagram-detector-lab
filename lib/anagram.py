58c431892cede1edf0a19fd6e21a5edb2ded0c4e
Walter N <2760walter@gmail.com>
2023-12-01 11:59:46 +0300

    complete

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,15 @@
 # your code goes here!
+class Anagram:
+
+  def __init__(self, word):
+    self.word = word
+
+  def match(self, list_of_words):
+    anagram = []
+    for listword in list_of_words:
+      if (sorted(self.word) == sorted(listword)):
+        anagram.append(listword)
+      else:
+          pass
+
+    return anagram
